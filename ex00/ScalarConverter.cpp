be760f656c018b6b4c1e82c85be2c9de6050ac84
Julen Leon Larrondo <jleon-la@c3r8s6.42urduliz.com>
2025-05-25 12:24:06 +0200

    I think it's done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -20,25 +20,47 @@ ScalarConverter::~ScalarConverter() {}
 template <typename T>
 static void	tochar(T& value)
 {
-	(void)value;
+	if (std::isnan(value) || value < std::numeric_limits<char>::min() || value > std::numeric_limits<char>::max())
+		std::cout << "char: impossible" << std::endl;
+	else if (std::isprint(static_cast<char>(value)) == false)
+		std::cout << "char: non printable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(value) << std::endl;
 }
 
 template <typename T>
 static void	toint(T& value)
 {
-	(void)value;
+	if (std::isnan(value) || value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max())
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
 }
 
 template <typename T>
 static void	tofloat(T& value)
 {
-	(void)value;
+	float f = static_cast<float>(value);
+
+	if (std::isinf(f) || std::isnan(f))
+		std::cout << "float: " << f << "f" << std::endl;
+	else if (value < -std::numeric_limits<float>::max() || value > std::numeric_limits<float>::max())
+		std::cout << "float: impossible" << std::endl;
+	else
+		std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
 }
 
 template <typename T>
 static void	todouble(T& value)
 {
-	(void)value;
+	double d = static_cast<double>(value);
+
+	if (std::isinf(d) || std::isnan(d))
+		std::cout << "double: " << d << std::endl;
+	else if (value < -std::numeric_limits<double>::max() || value > std::numeric_limits<double>::max())
+		std::cout << "double: impossible" << std::endl;
+	else
+		std::cout << std::fixed << std::setprecision(1) << "double: " << d << std::endl;
 }
 
 template <typename T>
